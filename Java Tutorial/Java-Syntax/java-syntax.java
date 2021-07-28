52a699e62978b9e8f746585598a7bccaa61c3522
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-07-27 23:09:27 -0300

    Java Variables Exercises

--- Java Tutorial/Java-Syntax/java-syntax.java
+++ Java Tutorial/Java-Syntax/java-syntax.java
@@ -1,7 +1,10 @@
 public class java-syntax {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World");
     }
+//        This is a single-line comment
 
+        /* This is a multi-line comment */
 }
