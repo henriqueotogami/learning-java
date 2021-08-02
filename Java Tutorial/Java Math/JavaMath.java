8cd56278916eeb44a045fd795240df60ed04dbac
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-08-01 22:56:37 -0300

    Java Math

---
+++ Java Tutorial/Java Math/JavaMath.java
@@ -0,0 +1,20 @@
+public class JavaMath {
+
+    static void exerciseOne(String ) {
+        // Use the correct method to find the highest value of x and y.
+        int x = 5;
+        int y = 10;
+        Math.max(x, y);
+    }
+
+    static void exerciseTwo (String[] args) {
+        // Use the correct method to find the square root of x.
+        int x = 16;
+        Math.sqrt(x);
+    }
+
+    static void exerciseThree(String[] args) {
+        // Use the correct method to return a random number between 0 (inclusive), and 1 (exclusive).
+        Math.random();
+    }
+}
