0c132c9ba7146c946ca858082ff7876717e32a06
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-08-15 21:04:10 -0300

    Testes

--- Henrique/src/Registers.java
+++ Henrique/src/Registers.java
@@ -1,54 +1,75 @@
-import java.util.Vector;
+
 
 public class Registers {
 
     public String[] complexDateStruct(final byte[] registers) {
 
         final int complexArrayLength = registers.length;
-        final int complexArrayStandardSize = 22;
-        final byte complexArrayStandardInfoDate = (byte) 0x0C;
-        String[] complexArray;
-        int[] getInfoDateDLMS = new int[0];
+//        final int complexArrayStandardSize = 22;
+//        final byte complexArrayStandardInfoDate = (byte) 0x0C;
+//        String[] complexArray;
+        int[] getInfoDateDLMS = new int[100];
         int indexInfoDateDLMS;
         int index = 0;
 
-        String getYearDLMS = null;
-        String getDayDLMS = null;
-        String getMonthDLMS = null;
-        String getHourDLMS = null;
-        String getMinutesDLMS = null;
-        String getSecondsDLMS = null;
-        String getRegStatusDLMS = null;
-        String getEventReleDLMS = null;
+        byte[] getYearDLMS = new byte[0];
+        byte[] getDayDLMS = new byte[0];
+        byte[] getMonthDLMS  = new byte[0];
+        byte[] getHourDLMS = new byte[0];
+        byte[] getMinutesDLMS = new byte[0];
+        byte[] getSecondsDLMS = new byte[0];
+        byte[] getRegStatusDLMS = new byte[0];
+        byte[] getEventReleDLMS = new byte[0];
 
-        final Vector eventZeus = new Vector();
+//        final Vector eventZeus = new Vector();
 
         for(indexInfoDateDLMS = 0; complexArrayLength > indexInfoDateDLMS; indexInfoDateDLMS++) {
             if ((registers[indexInfoDateDLMS] == (byte) 0x0C) && (registers[indexInfoDateDLMS + 10] == (byte) 0x80)) {
                 getInfoDateDLMS[index] = indexInfoDateDLMS;
-                index++;
+                index  = index + 1;
             }
         }
 
-        for (index = 0; getInfoDateDLMS.length > index; index++) {
+        final int eight = 8;
+        int indexx = 0;
 
+       for (int i = 0; eight > i; i++) {
+           getYearDLMS[i] = (byte) (registers[(getInfoDateDLMS[index] + 1)] + registers[(getInfoDateDLMS[index] + 2)]);
+           getDayDLMS[i] = registers[(getInfoDateDLMS[index] + 3)];
+           getMonthDLMS[i] = (registers[(getInfoDateDLMS[index] + 4)]);
+           getHourDLMS[i] = (registers[(getInfoDateDLMS[index] + 6)]);
+           getMinutesDLMS[i] = (registers[(getInfoDateDLMS[index] + 7)]);
+           getSecondsDLMS[i] = (registers[(getInfoDateDLMS[index] + 8)]);
+           getRegStatusDLMS[i] = (registers[(getInfoDateDLMS[index] + 14)]);
+           getEventReleDLMS[i] = (registers[(getInfoDateDLMS[index] + 16)]);
+           indexx = indexx + 1;
+       }
             // TODO Transformar em um array de bytes completo, antes de usar o ByteArray
-            getYearDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 1] + registers[indexInfoDateDLMS + 2]),4);
-            getDayDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 3]),2);
-            getMonthDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 4]),2);
-            getHourDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 6]),2);
-            getMinutesDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 7]),2);
-            getSecondsDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 8]),2);
-            getRegStatusDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 14]),2);
-            getEventReleDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 16]),2);
-        }
 
-        complexArray = new String[] {
-                ((getYearDLMS + "/" + getDayDLMS + "/" + getMonthDLMS + "/" + "" +
-                        getHourDLMS + ":" + getMinutesDLMS + ":" + getSecondsDLMS)),
-                getRegStatusDLMS, getEventReleDLMS };
+//            byte[] arrayDateTime = new byte [8];
 
-        return complexArray;
+        String[] arrayDateTime = new String[100];
+
+       for (int i = 0; i < getInfoDateDLMS.length; i++){
+           arrayDateTime[i] = ByteArray.byteToDec(getYearDLMS, i,1);
+           arrayDateTime[i + 1] = ByteArray.byteToDec(getDayDLMS, i,1);
+           arrayDateTime[i + 2] = ByteArray.byteToDec(getMonthDLMS, i,1);
+           arrayDateTime[i + 3] = ByteArray.byteToDec(getHourDLMS, i,1);
+           arrayDateTime[i + 4] = ByteArray.byteToDec(getMinutesDLMS, i,1);
+           arrayDateTime[i + 5] = ByteArray.byteToDec(getSecondsDLMS, i,1);
+           arrayDateTime[i + 6] = ByteArray.byteToDec(getRegStatusDLMS, i,1);
+           arrayDateTime[i + 7] = ByteArray.byteToDec(getEventReleDLMS, i,1);
+       }
+//            getYearDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 1] + registers[indexInfoDateDLMS + 2]),4);
+//            getDayDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 3]),2);
+//            getMonthDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 4]),2);
+//            getHourDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 6]),2);
+//            getMinutesDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 7]),2);
+//            getSecondsDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 8]),2);
+//            getRegStatusDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 14]),2);
+//            getEventReleDLMS = ByteArray.byteToDec((byte) (registers[indexInfoDateDLMS + 16]),2);
+//
+        return arrayDateTime;
     }
 }
 
