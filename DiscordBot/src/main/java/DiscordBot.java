8e052037b9be4b7c5fd9f9a3e60463b6bec7de66
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-09-08 22:38:07 -0300

    Mini projeto 19 - DiscordBot: Problemas com o jcenter() descontinuado

---
+++ DiscordBot/src/main/java/DiscordBot.java
@@ -0,0 +1,34 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+import java.io.IOException;
+
+public class DiscordBot {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String tokenReader = "423100622ba961f03d7ce443aec228a83f46f839baf3eee1d17f9746598d6c35";
+
+//        try {
+//            String filePath = "/Users/henriquematheusalvespereira/GitHub/learning-java/Henrique/DiscordBot.txt";
+//            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+//            String lineRead = "";
+//
+//            while((lineRead = bufferedReader.readLine()) != null) {
+//                tokenReader += lineRead;
+//            }
+//        } catch (FileNotFoundException exception) {
+//            exception.printStackTrace();
+//        }
+
+//        System.out.println(tokenReader);
+
+        builder.setToken(tokenReader);
+        builder.buildAsync();
+    }
+}
